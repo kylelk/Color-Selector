9d2b3babdee951d3d97dba15b840a7ee097c3585
kyle kersey <kylelk@comcast.net>
2014-10-24 19:41:49 -0700

    added code

--- Color Selector/Form1.Designer.vb
+++ Color Selector/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class colorSelectForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,152 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.rBar = New System.Windows.Forms.TrackBar()
+        Me.rNum = New System.Windows.Forms.NumericUpDown()
+        Me.gNum = New System.Windows.Forms.NumericUpDown()
+        Me.gBar = New System.Windows.Forms.TrackBar()
+        Me.bNum = New System.Windows.Forms.NumericUpDown()
+        Me.bBar = New System.Windows.Forms.TrackBar()
+        Me.captionTxt = New System.Windows.Forms.Label()
+        Me.colorInfo = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.hexColor = New System.Windows.Forms.Label()
+        CType(Me.rBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.rNum, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gNum, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bNum, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.colorInfo.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'rBar
+        '
+        Me.rBar.BackColor = System.Drawing.Color.Red
+        Me.rBar.LargeChange = 8
+        Me.rBar.Location = New System.Drawing.Point(13, 78)
+        Me.rBar.Maximum = 255
+        Me.rBar.Name = "rBar"
+        Me.rBar.Size = New System.Drawing.Size(359, 45)
+        Me.rBar.TabIndex = 0
+        '
+        'rNum
+        '
+        Me.rNum.Location = New System.Drawing.Point(378, 88)
+        Me.rNum.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.rNum.Name = "rNum"
+        Me.rNum.Size = New System.Drawing.Size(44, 20)
+        Me.rNum.TabIndex = 1
+        '
+        'gNum
+        '
+        Me.gNum.Location = New System.Drawing.Point(378, 124)
+        Me.gNum.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.gNum.Name = "gNum"
+        Me.gNum.Size = New System.Drawing.Size(44, 20)
+        Me.gNum.TabIndex = 3
+        '
+        'gBar
+        '
+        Me.gBar.BackColor = System.Drawing.Color.Green
+        Me.gBar.LargeChange = 8
+        Me.gBar.Location = New System.Drawing.Point(13, 114)
+        Me.gBar.Maximum = 255
+        Me.gBar.Name = "gBar"
+        Me.gBar.Size = New System.Drawing.Size(359, 45)
+        Me.gBar.TabIndex = 2
+        '
+        'bNum
+        '
+        Me.bNum.Location = New System.Drawing.Point(378, 162)
+        Me.bNum.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.bNum.Name = "bNum"
+        Me.bNum.Size = New System.Drawing.Size(44, 20)
+        Me.bNum.TabIndex = 5
+        '
+        'bBar
+        '
+        Me.bBar.BackColor = System.Drawing.Color.Blue
+        Me.bBar.LargeChange = 8
+        Me.bBar.Location = New System.Drawing.Point(13, 151)
+        Me.bBar.Maximum = 255
+        Me.bBar.Name = "bBar"
+        Me.bBar.Size = New System.Drawing.Size(359, 45)
+        Me.bBar.TabIndex = 4
+        '
+        'captionTxt
+        '
+        Me.captionTxt.AutoSize = True
+        Me.captionTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.captionTxt.Location = New System.Drawing.Point(117, 9)
+        Me.captionTxt.Name = "captionTxt"
+        Me.captionTxt.Size = New System.Drawing.Size(200, 31)
+        Me.captionTxt.TabIndex = 6
+        Me.captionTxt.Text = "Color Selector"
+        '
+        'colorInfo
+        '
+        Me.colorInfo.Controls.Add(Me.hexColor)
+        Me.colorInfo.Controls.Add(Me.Label1)
+        Me.colorInfo.Location = New System.Drawing.Point(13, 202)
+        Me.colorInfo.Name = "colorInfo"
+        Me.colorInfo.Size = New System.Drawing.Size(359, 48)
+        Me.colorInfo.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hex color:"
+        '
+        'hexColor
+        '
+        Me.hexColor.AutoSize = True
+        Me.hexColor.Location = New System.Drawing.Point(95, 18)
+        Me.hexColor.Name = "hexColor"
+        Me.hexColor.Size = New System.Drawing.Size(50, 13)
+        Me.hexColor.TabIndex = 1
+        Me.hexColor.Text = "#000000"
+        '
+        'colorSelectForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(434, 262)
+        Me.Controls.Add(Me.colorInfo)
+        Me.Controls.Add(Me.captionTxt)
+        Me.Controls.Add(Me.bNum)
+        Me.Controls.Add(Me.bBar)
+        Me.Controls.Add(Me.gNum)
+        Me.Controls.Add(Me.gBar)
+        Me.Controls.Add(Me.rNum)
+        Me.Controls.Add(Me.rBar)
+        Me.Name = "colorSelectForm"
+        Me.Text = "Color Selector"
+        CType(Me.rBar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.rNum, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gNum, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gBar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bNum, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.colorInfo.ResumeLayout(False)
+        Me.colorInfo.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents rBar As System.Windows.Forms.TrackBar
+    Friend WithEvents rNum As System.Windows.Forms.NumericUpDown
+    Friend WithEvents gNum As System.Windows.Forms.NumericUpDown
+    Friend WithEvents gBar As System.Windows.Forms.TrackBar
+    Friend WithEvents bNum As System.Windows.Forms.NumericUpDown
+    Friend WithEvents bBar As System.Windows.Forms.TrackBar
+    Friend WithEvents captionTxt As System.Windows.Forms.Label
+    Friend WithEvents colorInfo As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents hexColor As System.Windows.Forms.Label
 
 End Class
--- Color Selector/Form1.vb
+++ Color Selector/Form1.vb
@@ -1,3 +1,70 @@
-﻿Public Class Form1
+﻿''' <summary>
+''' Structure holding a RGB value
+''' </summary>
+''' <param name="r">Red value as 8 bit byte</param>
+''' <param name="g">Green value as 8 bit byte</param>
+''' <param name="b">Blue value as 8 bit byte</param>
+''' <remarks></remarks>
+Public Structure colorHolder
+    Public r As Byte
+    Public g As Byte
+    Public b As Byte
+End Structure
+
+Public Class colorSelectForm
+
+    Public colorValue As New colorHolder
+
+    Function inverseColor(colorVal As colorHolder) As colorHolder
+        Dim result As colorHolder
+        result.r = 255 - colorVal.r
+        result.g = 255 - colorVal.b
+        result.b = 255 - colorVal.b
+        Return result
+    End Function
+
+    Public Sub update_text(colorVal As colorHolder)
+        Me.BackColor = Color.FromArgb(colorVal.r, colorVal.g, colorVal.b)
+        Dim invColor As colorHolder = inverseColor(colorVal)
+        captionTxt.ForeColor = Color.FromArgb(invColor.r, invColor.g, invColor.b)
+        colorInfo.ForeColor = Color.FromArgb(invColor.r, invColor.g, invColor.b)
+        hexColor.Text = Color.FromArgb(invColor.r, invColor.g, invColor.b).ToString
+    End Sub
+
+    Private Sub colorBar_changed(sender As Object, e As EventArgs) Handles rBar.Scroll, gBar.Scroll, bBar.Scroll
+
+        If sender Is rBar Then
+            Me.colorValue.r = rBar.Value
+            rNum.Value = Me.colorValue.r
+        End If
+        If sender Is gBar Then
+            Me.colorValue.g = gBar.Value
+            gNum.Value = Me.colorValue.g
+        End If
+        If sender Is bBar Then
+            Me.colorValue.b = bBar.Value
+            bNum.Value = Me.colorValue.b
+        End If
+
+        update_text(colorValue)
+    End Sub
+
+    Private Sub colorNum_changed(sender As Object, e As EventArgs) Handles rNum.ValueChanged, gNum.ValueChanged, bNum.ValueChanged
+        If sender Is rNum Then
+            Me.colorValue.r = rNum.Value
+            rBar.Value = Me.colorValue.r
+        End If
+        If sender Is gNum Then
+            Me.colorValue.g = gNum.Value
+            gBar.Value = Me.colorValue.g
+        End If
+        If sender Is bNum Then
+            Me.colorValue.b = bNum.Value
+            bBar.Value = Me.colorValue.b
+        End If
+
+        update_text(colorValue)
+    End Sub
+
 
 End Class
